fb2494b0c04f7fa38bdf8fd3b4d8a08c385698a8
root <tryotto1@gmail.com>
2020-09-21 12:25:41 +0900

    refactoring 9/21

--- project_refactorize/src/data/train_dataset.py
+++ project_refactorize/src/data/train_dataset.py
@@ -23,25 +23,3 @@ class TrainDataset(Dataset):
         label = self.df['class'][idx]
 
         return image, label
-
-'''
-class TrainDataset(Dataset):
-    def __init__(self, df, mode='train', transforms=None):
-        self.df = df
-        self.mode = mode
-        self.transform = transforms[self.mode]
-        
-    def __len__(self):
-        return len(self.df)
-            
-    def __getitem__(self, idx):
-        
-        image = Image.open(TRAIN_IMAGE_PATH / self.df['img_file'][idx]).convert("RGB")
-
-        if self.transform:
-            image = self.transform(image)
-
-        label = self.df['class'][idx]
-
-        return image, label
-'''
